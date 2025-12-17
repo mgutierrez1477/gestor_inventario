d4f7d90d1740b08fdbfffcce60592686ca2f8c4b
mgutierrez1477 <mgutierrez1477@gmail.com>
2025-12-16 20:44:20 -0600

    configurando serializer

--- inventario/admin.py
+++ inventario/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Product
 
-# Register your models here.
+
+class ProductAdmin(admin.ModelAdmin):
+
+    list_display = ("id","name", "description", "price", "stock", "created_at")
+    list_filter = ("created_at",)
+    search_fields = ("name", "description")
+    list_editable = ("price", "stock")
+
+admin.site.register(Product, ProductAdmin)
--- inventario/models.py
+++ inventario/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.core.validators import MinValueValidator
 
-# Create your models here.
+
+class Product(models.Model):
+    name = models.CharField(max_length=50, null=False, blank=False, verbose_name="Producto")
+    description = models.TextField(max_length=300, null=False, blank=False, verbose_name="Descripcion")
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    
+    stock = models.IntegerField(
+        #validar que el stock siempre sea positivo en toda la app
+        validators=[MinValueValidator(0)]
+    )
+    created_at = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
