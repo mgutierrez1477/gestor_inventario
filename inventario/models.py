d5bfdafd9ab0bcd208401e5579b0b42f3b5e545f
mgutierrez1477 <mgutierrez1477@gmail.com>
2025-12-17 20:04:23 -0600

    serializer terminado

--- inventario/models.py
+++ inventario/models.py
@@ -3,15 +3,18 @@ from django.core.validators import MinValueValidator
 
 
 class Product(models.Model):
-    name = models.CharField(max_length=50, null=False, blank=False, verbose_name="Producto")
-    description = models.TextField(max_length=300, null=False, blank=False, verbose_name="Descripcion")
-    price = models.DecimalField(max_digits=10, decimal_places=2)
+    name = models.CharField(max_length=50, blank=False, verbose_name="Producto")
+    description = models.TextField(max_length=300,  blank=False, verbose_name="Descripcion")
+    price = models.DecimalField(max_digits=10, decimal_places=2,
+                                validators=[MinValueValidator(0)])
     
-    stock = models.IntegerField(
-        #validar que el stock siempre sea positivo en toda la app
-        validators=[MinValueValidator(0)]
-    )
-    created_at = models.DateField(auto_now_add=True)
+    stock = models.PositiveIntegerField(verbose_name="Cantidad")
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Fecha de creacion")
+
+    class Meta:
+        ordering = ["-created_at", "name"]
+        verbose_name = "Producto"
+        verbose_name_plural = "Productos"
 
     def __str__(self):
         return self.name
--- inventario/serializers.py
+++ inventario/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 from .models import Product
 
+#el serializer es un convierte nuestros modelos django a modelos json para las api
 class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
@@ -12,3 +13,5 @@ class ProductSerializer(serializers.ModelSerializer):
             "stock",
             "created_at"
         ]
+
+        read_only_fields = ("id", "created_at")
