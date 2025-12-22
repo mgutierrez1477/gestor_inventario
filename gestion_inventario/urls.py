5db87d07b2ea88cd76dc01bec4e02a7eeac70418
mgutierrez1477 <mgutierrez1477@gmail.com>
2025-12-22 15:22:11 -0600

    fix swagger

--- gestion_inventario/urls.py
+++ gestion_inventario/urls.py
@@ -10,18 +10,15 @@ from drf_spectacular.views import (
 from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("admin/", admin.site.urls),
+
+    path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
+    path(
+        "api/docs/",
+        SpectacularSwaggerView.as_view(url_name="schema"),
+        name="swagger-ui",
+    ),
+
     path("api/", include("inventario.urls")),
-    #recibe la info del usuario y genera un token
-    path('api/token/', obtain_auth_token),
-    path('api-auth/', include('rest_framework.urls')),
-
-    #openApi schema
-    path('api/schema/', SpectacularAPIView.as_view(), name="schema"),
-
-    # swagger UI
-    path('api/docs/', SpectacularSwaggerView.as_view(url_name="schema"), name="swagger-ui"),
-
-    #redoc
-    path('api/redoc/', SpectacularRedocView.as_view(url_name="schema"), name="redoc")
+    path("api/token/", obtain_auth_token),
 ]
